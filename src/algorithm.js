1618d3532aeb5d4e42bdd00a49fc2ffe18a5d0b1
Tsur <prucheta@gmail.com>
2015-05-17 23:31:24 +0200

    added test cases

--- src/algorithm.js
+++ src/algorithm.js
@@ -8,354 +8,399 @@ var topPointer = 1;
 var bottomPointer = 1;
 var middlePointer = 1;
 
-export function init(state) {
+export
 
-    actualState = [];
+function init(state) {
+
+  return new Array();
 
 }
 
-export function getRedrawList(){
+export
+
+function convertToPOJO(state) {
+
+  if (!_.isArray(state) || _.isEmpty(state)) {
+
+    return new Object();
+
+  }
+
+  state = _.filter(state, (e) => _.isArray(e) && !_.isEmpty(e));
+
+  return _.zipObject(_.map(state, (e) => _.first(e)), _.map(state, (e) => _.uniq(_.sortBy(_.drop(e)))));
+
+}
+
+export
+
+function getRedrawList() {
 
   return redrawList;
 
 };
 
-export function nextGeneration() {
+export
 
-    let x, y, i, j, m, n, key, t1, t2, alive = 0, neighbours, deadNeighbours, allDeadNeighbours = {}, newState = [];
+function nextGeneration() {
 
-    redrawList = [];
+  let x, y, i, j, m, n, key, t1, t2, alive = 0,
+    neighbours, deadNeighbours, allDeadNeighbours = {},
+    newState = [];
 
-    for (i = 0; i < actualState.length; i++) {
-      
-      topPointer = 1;
-      bottomPointer = 1;
-                
+  redrawList = [];
+
+  for (i = 0; i < actualState.length; i++) {
+
+    topPointer = 1;
+    bottomPointer = 1;
+
+    for (j = 1; j < actualState[i].length; j++) {
+
+      x = actualState[i][j];
+      y = actualState[i][0];
+
+      // Possible dead neighbours
+      deadNeighbours = [
+        [x - 1, y - 1, 1],
+        [x, y - 1, 1],
+        [x + 1, y - 1, 1],
+        [x - 1, y, 1],
+        [x + 1, y, 1],
+        [x - 1, y + 1, 1],
+        [x, y + 1, 1],
+        [x + 1, y + 1, 1]
+      ];
+
+      // Get number of live neighbours and remove alive neighbours from deadNeighbours
+      neighbours = getNeighboursFromAlive(x, y, i, deadNeighbours);
+
+      // Join dead neighbours to check list
+      for (m = 0; m < 8; m++) {
+
+        if (deadNeighbours[m] !== undefined) {
+
+          key = deadNeighbours[m][0] + ',' + deadNeighbours[m][1]; // Create hashtable key
+
+          if (allDeadNeighbours[key] === undefined) {
+            allDeadNeighbours[key] = 1;
+          } else {
+            allDeadNeighbours[key]++;
+          }
+
+        }
+
+      }
+
+      if (!(neighbours === 0 || neighbours === 1 || neighbours > 3)) {
+
+        addCell(x, y, newState);
+        alive++;
+        redrawList.push([x, y, 2]); // Keep alive
+
+      } else {
+
+        redrawList.push([x, y, 0]); // Kill cell
+      }
+
+    }
+  }
+
+  // Process dead neighbours
+  for (key in allDeadNeighbours) {
+
+    if (allDeadNeighbours[key] === 3) { // Add new Cell
+
+      key = key.split(',');
+      t1 = parseInt(key[0], 10);
+      t2 = parseInt(key[1], 10);
+
+      addCell(t1, t2, newState);
+      alive++;
+      redrawList.push([t1, t2, 1]);
+
+    }
+
+  }
+
+  actualState = newState;
+
+  return alive;
+}
+
+export
+
+function getNeighboursFromAlive(x, y, i, possibleNeighboursList) {
+
+  var neighbours = 0,
+    k;
+
+  // Top
+  if (actualState[i - 1] !== undefined) {
+
+    if (actualState[i - 1][0] === (y - 1)) {
+
+      for (k = topPointer; k < actualState[i - 1].length; k++) {
+
+        if (actualState[i - 1][k] >= (x - 1)) {
+
+          if (actualState[i - 1][k] === (x - 1)) {
+            possibleNeighboursList[0] = undefined;
+            topPointer = k + 1;
+            neighbours++;
+          }
+
+          if (actualState[i - 1][k] === x) {
+            possibleNeighboursList[1] = undefined;
+            topPointer = k;
+            neighbours++;
+          }
+
+          if (actualState[i - 1][k] === (x + 1)) {
+            possibleNeighboursList[2] = undefined;
+
+            if (k == 1) {
+              topPointer = 1;
+            } else {
+              topPointer = k - 1;
+            }
+
+            neighbours++;
+          }
+
+          if (actualState[i - 1][k] > (x + 1)) {
+            break;
+          }
+        }
+
+      }
+    }
+  }
+
+  // Middle
+  for (k = 1; k < actualState[i].length; k++) {
+
+    if (actualState[i][k] >= (x - 1)) {
+
+      if (actualState[i][k] === (x - 1)) {
+        possibleNeighboursList[3] = undefined;
+        neighbours++;
+      }
+
+      if (actualState[i][k] === (x + 1)) {
+        possibleNeighboursList[4] = undefined;
+        neighbours++;
+      }
+
+      if (actualState[i][k] > (x + 1)) {
+        break;
+      }
+    }
+
+  }
+
+  // Bottom
+  if (actualState[i + 1] !== undefined) {
+
+    if (actualState[i + 1][0] === (y + 1)) {
+
+      for (k = bottomPointer; k < actualState[i + 1].length; k++) {
+
+        if (actualState[i + 1][k] >= (x - 1)) {
+
+          if (actualState[i + 1][k] === (x - 1)) {
+            possibleNeighboursList[5] = undefined;
+            bottomPointer = k + 1;
+            neighbours++;
+          }
+
+          if (actualState[i + 1][k] === x) {
+            possibleNeighboursList[6] = undefined;
+            bottomPointer = k;
+            neighbours++;
+          }
+
+          if (actualState[i + 1][k] === (x + 1)) {
+            possibleNeighboursList[7] = undefined;
+
+            if (k == 1) {
+              bottomPointer = 1;
+            } else {
+              bottomPointer = k - 1;
+            }
+
+            neighbours++;
+          }
+
+          if (actualState[i + 1][k] > (x + 1)) {
+            break;
+          }
+
+        }
+
+      }
+
+    }
+
+  }
+
+  return neighbours;
+
+}
+
+export
+
+function isAlive(x, y) {
+
+  var i, j;
+
+  for (i = 0; i < actualState.length; i++) {
+
+    if (actualState[i][0] === y) {
+
       for (j = 1; j < actualState[i].length; j++) {
 
-        x = actualState[i][j];
-        y = actualState[i][0];
-
-        // Possible dead neighbours
-        deadNeighbours = [[x-1, y-1, 1], [x, y-1, 1], [x+1, y-1, 1], [x-1, y, 1], [x+1, y, 1], [x-1, y+1, 1], [x, y+1, 1], [x+1, y+1, 1]];
-
-        // Get number of live neighbours and remove alive neighbours from deadNeighbours
-        neighbours = getNeighboursFromAlive(x, y, i, deadNeighbours);
-
-        // Join dead neighbours to check list
-        for (m = 0; m < 8; m++) {
-          
-          if (deadNeighbours[m] !== undefined) {
-
-            key = deadNeighbours[m][0] + ',' + deadNeighbours[m][1]; // Create hashtable key
-            
-            if (allDeadNeighbours[key] === undefined) {
-              allDeadNeighbours[key] = 1;
-            } else {
-              allDeadNeighbours[key]++;
-            }
-
-          }
-
+        if (actualState[i][j] === x) {
+          return true;
         }
-
-        if (!(neighbours === 0 || neighbours === 1 || neighbours > 3)) {
-          
-          addCell(x, y, newState);
-          alive++;
-          redrawList.push([x, y, 2]); // Keep alive
-
-        } else {
-          
-          redrawList.push([x, y, 0]); // Kill cell
-        }
-
-      }
-    }
-
-    // Process dead neighbours
-    for (key in allDeadNeighbours) {
-
-      if (allDeadNeighbours[key] === 3) { // Add new Cell
-        
-        key = key.split(',');
-        t1 = parseInt(key[0], 10);
-        t2 = parseInt(key[1], 10);
-        
-        addCell(t1, t2, newState);
-        alive++;
-        redrawList.push([t1, t2, 1]);
 
       }
 
     }
 
-    actualState = newState;
+  }
 
-    return alive;
+  return false;
 }
 
-export function getNeighboursFromAlive(x, y, i, possibleNeighboursList) {
-    
-    var neighbours = 0, k;
+export
 
-    // Top
-    if (actualState[i-1] !== undefined) {
+function removeCell(x, y, state) {
 
-        if (actualState[i-1][0] === (y - 1)) {
-
-            for (k = topPointer; k < actualState[i-1].length; k++) {
-
-                if (actualState[i-1][k] >= (x-1) ) {
-
-                    if (actualState[i-1][k] === (x - 1)) {
-                      possibleNeighboursList[0] = undefined;
-                      topPointer = k + 1;
-                      neighbours++;
-                    }
-
-                    if (actualState[i-1][k] === x) {
-                      possibleNeighboursList[1] = undefined;
-                      topPointer = k;
-                      neighbours++;
-                    }
-
-                    if (actualState[i-1][k] === (x + 1)) {
-                      possibleNeighboursList[2] = undefined;
-
-                      if (k == 1) {
-                        topPointer = 1;
-                      } else {
-                        topPointer = k - 1;
-                      }
-                                        
-                      neighbours++;
-                    }
-
-                    if (actualState[i-1][k] > (x + 1)) {
-                      break;
-                    }
-                }
-
-            }
-        }
-    }
-    
-    // Middle
-    for (k = 1; k < actualState[i].length; k++) {
-
-        if (actualState[i][k] >= (x - 1)) {
-
-            if (actualState[i][k] === (x - 1)) {
-              possibleNeighboursList[3] = undefined;
-              neighbours++;
-            }
-
-            if (actualState[i][k] === (x + 1)) {
-              possibleNeighboursList[4] = undefined;
-              neighbours++;
-            }
-
-            if (actualState[i][k] > (x + 1)) {
-              break;
-            }
-        }
-
-    }
-
-    // Bottom
-    if (actualState[i+1] !== undefined) {
-
-      if (actualState[i+1][0] === (y + 1)) {
-
-        for (k = bottomPointer; k < actualState[i+1].length; k++) {
-
-          if (actualState[i+1][k] >= (x - 1)) {
-
-            if (actualState[i+1][k] === (x - 1)) {
-              possibleNeighboursList[5] = undefined;
-              bottomPointer = k + 1;
-              neighbours++;
-            }
-
-            if (actualState[i+1][k] === x) {
-              possibleNeighboursList[6] = undefined;
-              bottomPointer = k;
-              neighbours++;
-            }
-
-            if (actualState[i+1][k] === (x + 1)) {
-              possibleNeighboursList[7] = undefined;
-                                
-              if (k == 1) {
-                bottomPointer = 1;
-              } else {
-                bottomPointer = k - 1;
-              }
-
-              neighbours++;
-            }
-
-            if (actualState[i+1][k] > (x + 1)) {
-              break;
-            }
-
-          }
-
-        }
-
-      }
-
-    }
-    
-    return neighbours;
-
-}
-
-export function isAlive(x, y) {
-
-    var i, j;
-
-    for (i = 0; i < actualState.length; i++) {
-
-      if (actualState[i][0] === y) {
-
-        for (j = 1; j < actualState[i].length; j++) {
-
-          if (actualState[i][j] === x) {
-            return true;
-          }
-
-        }
-
-      }
-
-    }
-
-    return false;
-}
-
-export function removeCell(x, y, state) {
-    
-    var i, j;
-
-    state = state || actualState;
-
-    for (i = 0; i < state.length; i++) {
-      
-      if (state[i][0] === y) {
-
-        if (state[i].length === 2) { // Remove all Row
-          
-          state.splice(i, 1);
-
-        } else { // Remove Element
-          
-          for (j = 1; j < state[i].length; j++) {
-            
-            if (state[i][j] === x) {
-              state[i].splice(j, 1);
-            }
-
-          }
-
-        }
-
-      }
-
-    }
-}
-
-export function addCell(x, y, state) {
+  var i, j;
 
   state = state || actualState;
 
-        if (state.length === 0) {
+  for (i = 0; i < state.length; i++) {
 
-          state.push([y, x]);
-          return;
+    if (state[i][0] === y) {
 
-        }
+      if (state[i].length === 2) { // Remove all Row
 
-        let k, n, m, tempRow, newState = [], added;
+        state.splice(i, 1);
 
-        if (y < state[0][0]) { // Add to Head
+      } else { // Remove Element
 
-          newState = [[y,x]];
+        for (j = 1; j < state[i].length; j++) {
 
-          for (k = 0; k < state.length; k++) {
-            newState[k+1] = state[k];
-          }
-
-          for (k = 0; k < newState.length; k++) {
-            state[k] = newState[k];
-          }
-
-          return;
-
-        } else if (y > state[state.length - 1][0]) { // Add to Tail
-
-          state[state.length] = [y, x];
-          return;
-
-        } else { // Add to Middle
-
-          for (n = 0; n < state.length; n++) {
-            
-            if (state[n][0] === y) { // Level Exists
-              
-              tempRow = [];
-              added = false;
-              
-              for (m = 1; m < state[n].length; m++) {
-                
-                if ((!added) && (x < state[n][m])) {
-                  tempRow.push(x);
-                  added = !added;
-                }
-
-                tempRow.push(state[n][m]);
-              }
-
-              tempRow.unshift(y);
-              
-              if (!added) {
-                
-                tempRow.push(x);
-
-              }
-
-              state[n] = tempRow;
-              return;
-
-            }
-
-            if (y < state[n][0]) { // Create Level
-              
-              newState = [];
-              
-              for (k = 0; k < state.length; k++) {
-
-                if (k === n) {
-                  newState[k] = [y,x];
-                  newState[k+1] = state[k];
-                } else if (k < n) {
-                  newState[k] = state[k];
-                } else if (k > n) {
-                  newState[k+1] = state[k];
-                }
-
-              }
-
-              for (k = 0; k < newState.length; k++) {
-                state[k] = newState[k];
-              }
-
-              return;
-
-            }
-
+          if (state[i][j] === x) {
+            state[i].splice(j, 1);
           }
 
         }
+
+      }
+
+    }
+
+  }
+}
+
+export
+
+function addCell(x, y, state) {
+
+  state = state || actualState;
+
+  if (state.length === 0) {
+
+    state.push([y, x]);
+    return;
+
+  }
+
+  let k, n, m, tempRow, newState = [],
+    added;
+
+  if (y < state[0][0]) { // Add to Head
+
+    newState = [
+      [y, x]
+    ];
+
+    for (k = 0; k < state.length; k++) {
+      newState[k + 1] = state[k];
+    }
+
+    for (k = 0; k < newState.length; k++) {
+      state[k] = newState[k];
+    }
+
+    return;
+
+  } else if (y > state[state.length - 1][0]) { // Add to Tail
+
+    state[state.length] = [y, x];
+    return;
+
+  } else { // Add to Middle
+
+    for (n = 0; n < state.length; n++) {
+
+      if (state[n][0] === y) { // Level Exists
+
+        tempRow = [];
+        added = false;
+
+        for (m = 1; m < state[n].length; m++) {
+
+          if ((!added) && (x < state[n][m])) {
+            tempRow.push(x);
+            added = !added;
+          }
+
+          tempRow.push(state[n][m]);
+        }
+
+        tempRow.unshift(y);
+
+        if (!added) {
+
+          tempRow.push(x);
+
+        }
+
+        state[n] = tempRow;
+        return;
+
+      }
+
+      if (y < state[n][0]) { // Create Level
+
+        newState = [];
+
+        for (k = 0; k < state.length; k++) {
+
+          if (k === n) {
+            newState[k] = [y, x];
+            newState[k + 1] = state[k];
+          } else if (k < n) {
+            newState[k] = state[k];
+          } else if (k > n) {
+            newState[k + 1] = state[k];
+          }
+
+        }
+
+        for (k = 0; k < newState.length; k++) {
+          state[k] = newState[k];
+        }
+
+        return;
+
+      }
+
+    }
+
+  }
 }
--- test/algorithm.js
+++ test/algorithm.js
@@ -1,15 +1,104 @@
-import * as algorithm from '../src/algorithm';
+import {
+  init,
+  convertToPOJO
+}
+from '../src/algorithm';
 import should from 'should';
 
-export default function(){
+export
+default
+
+function() {
 
   describe('init method', () => {
-  
-      it('should init the state', () => {
-      
-        should(true).be.true;
 
+    it('should init the state', () => {
+
+      should(init()).be.Array;
+      should(init()).be.Empty;
+
+    });
+
+  });
+
+  describe('convertToPOJO method', () => {
+
+    it('should return {} on no input', () => {
+
+      should(convertToPOJO()).be.eql({});
+
+    });
+
+    it('should return {} on input != than Array', () => {
+
+      should(convertToPOJO(12)).be.eql({});
+      should(convertToPOJO(true)).be.eql({});
+      should(convertToPOJO("")).be.eql({});
+      should(convertToPOJO(.23)).be.eql({});
+      should(convertToPOJO({})).be.eql({});
+
+    });
+
+    it('should return {} on input != array of arrays', () => {
+
+      should(convertToPOJO([
+        []
+      ])).be.eql({});
+      should(convertToPOJO([1])).be.eql({});
+
+    });
+
+    it('should return POJO on input == array of arrays', () => {
+
+      should(convertToPOJO([
+        [1]
+      ])).be.eql({
+        '1': []
       });
+
+      should(convertToPOJO([
+        [1, 2]
+      ])).be.eql({
+        '1': [2]
+      });
+
+      should(convertToPOJO([
+        [1, 2, 3]
+      ])).be.eql({
+        '1': [2, 3]
+      });
+
+      should(convertToPOJO([
+        [1, 3, 2]
+      ])).be.eql({
+        '1': [2, 3]
+      });
+
+      should(convertToPOJO([
+        [1],
+        [2]
+      ])).be.eql({
+        '1': [],
+        '2': []
+      });
+
+      should(convertToPOJO([
+        [1],
+        [1, 2, 3],
+        [1]
+      ])).be.eql({
+        '1': []
+      });
+
+      should(convertToPOJO([
+        [50, 1, 10, 4],
+        [39, 100, 110]
+      ])).be.eql({
+        '39': [100, 110],
+        '50': [1, 4, 10]
+      });
+
+    });
 
   });
 
