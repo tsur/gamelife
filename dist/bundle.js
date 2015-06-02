e6cf0185d675fe2f4803bf38b8ce5e187edd0702
Tsur <prucheta@gmail.com>
2015-06-02 21:09:03 +0200

    fixed action icons position

--- dist/bundle.js
+++ dist/bundle.js
@@ -8855,7 +8855,6 @@ System.register("public/src/entry", ["public/src/game", "public/src/actions"], f
       window.onload = (function() {
         return runActions(runGame());
       });
-      window.onload();
     }
   };
 });
