aacd38cc2fdfc3464e94f4c4fa59e1db27e9b07d
Tsur <prucheta@gmail.com>
2015-06-04 21:53:58 +0200

    updated build process to store index in root folder

--- preprocess.js
+++ preprocess.js
@@ -3,7 +3,7 @@ var DataURI = require('datauri');
 var fs = require('fs');
 
 // Simple wrapper around fs.readFileSync and fs.writeFileSync
-preprocess.preprocessFileSync(__dirname + '/public/index.html', __dirname + '/index.html', {
+preprocess.preprocessFileSync(__dirname + '/public/index.html', __dirname + '/index-debug.html', {
   DEBUG: true
 });
 
@@ -13,7 +13,7 @@ var imgPlay = new DataURI().format('.png', fs.readFileSync(__dirname + '/public/
 var imgPause = new DataURI().format('.png', fs.readFileSync(__dirname + '/public/assets/pause.png'));
 var imgRestart = new DataURI().format('.png', fs.readFileSync(__dirname + '/public/assets/restart.png'));
 
-preprocess.preprocessFileSync(__dirname + '/public/index.html', __dirname + '/dist/index.html', {
+preprocess.preprocessFileSync(__dirname + '/public/index.html', __dirname + '/index.html', {
   DEBUG: false,
   IMG_PROFILE: imgProfile.content,
   IMG_EDIT: imgEdit.content,
--- public/src/entry.js
+++ public/src/entry.js
@@ -3,6 +3,5 @@ import runActions from './actions';
 
 // Kick it off after window object has loaded (all resources were loaded)
 window.onload = () => runActions(runGame());
-
 //@TODO: Remove for produccion
 window.onload();
